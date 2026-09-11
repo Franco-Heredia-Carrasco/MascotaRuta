51fa7066487122121c9ef3f16bda9e24bfa36952
TERMINAfh <C.lancellotti03@gmail.com>
2026-09-11 13:10:45 -0300

    segundo git

--- app/src/main/java/com/example/mascotaruta/MainActivity.java
+++ app/src/main/java/com/example/mascotaruta/MainActivity.java
@@ -6,10 +6,6 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,33 +15,25 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+
         Button btnRescatista = findViewById(R.id.button_rescatista);
-
         Button btnAdoptante = findViewById(R.id.button_adoptante);
-
         Button btnCasaTemporal = findViewById(R.id.button_temporal);
 
 
-        btnRescatista.setOnClickListener((V -> abrirFormulario
+        btnRescatista.setOnClickListener(v -> abrirFormulario());
+        btnAdoptante.setOnClickListener(v -> abrirFormulario());
+        btnCasaTemporal.setOnClickListener(v -> abrirFormulario());
 
 
-        private void abrirFormulario() {
+    }
+
+    private void abrirFormulario() {
         Intent intent = new Intent(MainActivity.this, RegistrarMascotaActivity.class);
-
-
-
-
-
-
-        };
-
+        startActivity(intent);
     }
+}
 
-
-
-
-
-    }
 
 
 
